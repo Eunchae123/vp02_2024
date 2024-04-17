9e5a2ee1a9a4d5d8dadd52a4966dfb67ee67b615
Eunchae123 <165130102+Eunchae123@users.noreply.github.com>
2024-04-17 14:41:52 +0900

    Add files via upload

---
+++ 022_dclock/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _022_dclock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void DrawHands(double radHr, double radMin, double radSec)
+        {
+            DrawLine(0,0,
+                (int)(hourHand * Math.Sin(radHr)),
+                (int)(hourHand * Math.Cos(radHr)),
+                Brushes.RoyalBlue,8);
+        }
+    }
+}
---
+++ test/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        
+        ComboBox[] grades;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+                   }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            string result = "";
+
+            
+            if (rb1.Checked)
+                result += "의료IT공학과\n";
+            else if (rb2.Checked)
+                result += "의공학과\n";
+            else if (rb3.Checked)
+                result += "의료신소재학과\n";
+            else if (rb4.Checked)
+                result += "제약생명공학과\n";
+            else if (rb5.Checked)
+                result += "의료공간디자인학과\n";
+            string userInput = textBox1.Text;
+            string numbers = textBox2.Text;
+                result += "이름: " + userInput + "\n";
+                result += "학번: " + numbers;
+
+            List<string> selectedSubjects = new List<string>();
+            if (cbPython.Checked)
+                selectedSubjects.Add(cbPython.Text);
+            if (cbC.Checked)
+                selectedSubjects.Add(cbC.Text);
+            if (cbCpp.Checked)
+                selectedSubjects.Add(cbCpp.Text);
+            if (cbCs.Checked)
+                selectedSubjects.Add(cbCs.Text);
+            if (cbJ.Checked)
+                selectedSubjects.Add(cbJ.Text);
+            result += string.Join(", ", selectedSubjects);
+
+
+
+
+            CheckBox[] checkBoxes = { cbPython, cbC, cbCpp, cbCs, cbJ };
+
+
+
+
+
+            MessageBox.Show(result, "학생정보");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            grades = new ComboBox[] { cb };
+            int[] arr = { 1, 2, 3, 4 };
+            CheckBox[] checkBoxes = { cbPython, cbC, cbCpp, cbCs, cbJ };
+
+
+            foreach (var cb in grades)
+            {
+                foreach (var i in arr)
+                    cb.Items.Add(i);
+                cb.SelectedItem = 3;
+            }
+            
+            }
+        }
+    }
